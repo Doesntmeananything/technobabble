449599ec7fb1d601f2c78d1ba8799be6833262e8
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-08-01 12:04:07 +0300

    Update message field

--- resources/migrations/20190726134612-add-messages-table.up.sql
+++ resources/migrations/20190726134612-add-messages-table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE messages
 (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   username VARCHAR(30) NOT NULL,
-  thought TEXT NOT NULL,
+  message TEXT NOT NULL,
   created TIMESTAMPTZ NOT NULL
 );
