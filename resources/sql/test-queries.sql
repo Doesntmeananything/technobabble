93f2ff6dc4d405d59406801a051c8d134839b521
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-08-05 21:23:16 +0300

    Add test queries

--- resources/sql/test-queries.sql
+++ resources/sql/test-queries.sql
@@ -1,23 +1,8 @@
--- :name clear-thoughts! :! :n
--- :doc DELETES ALL THOUGHTS!
-DELETE FROM thoughts;
+-- :name clear-messages! :! :n
+-- :doc DELETES ALL MESSAGES!
+DELETE FROM messages;
 
 -- :name wipe-database! :! :n
--- :doc DELETES ALL USERS, THOUGHTS AND THEIR RECORDS
-DELETE FROM thought_clusters_thought;
-DELETE FROM thought_clusters;
-DELETE FROM reminders;
-DELETE FROM thoughts;
+-- :doc DELETES ALL USERS
 DELETE FROM users_roles;
 DELETE FROM users;
-
-
--- :name update-thought-created! :! :n
--- :doc Forces update for a thought's create date, something we will not allow in the real code.
-UPDATE thoughts
-SET created = :created
-WHERE id = :id;
-
--- :name get-all-reminders :? :*
--- :doc Returns all reminders in the database
-SELECT * FROM reminders ORDER BY created;
